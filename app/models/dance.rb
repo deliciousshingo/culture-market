12656e36271a4b661a8d2c00a1b8258108c6456d
deliciousshingo <67893129+deliciousshingo@users.noreply.github.com>
2020-09-26 01:02:14 +0900

    アソシエーション、バリデーション設定

--- app/models/dance.rb
+++ app/models/dance.rb
@@ -1,2 +1,10 @@
 class Dance < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+
+  validates :title, :introduce, :price, :reservation_time, :genre, presence: true
+
+  validates :genre_id, numericality: { other_than: 1 } 
 end
